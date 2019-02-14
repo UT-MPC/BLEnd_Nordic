e1a8f9f408d2b719eb457be5e2741660a80f6506
liuchg <liuchg@utexas.edu>
2019-02-14 13:03:58 -0600

    Updated to use RMS for sound level

--- src/blend_project_templates/Thingy_IoT_SensorKit_v2.1.0/percom_demo/drv_audio_sound_level.c
+++ src/blend_project_templates/Thingy_IoT_SensorKit_v2.1.0/percom_demo/drv_audio_sound_level.c
@@ -23,19 +23,19 @@ void drv_audio_coder_init(void) {
 
 void drv_audio_coder_encode(int16_t *raw_samples, m_audio_frame_t *p_frame) {
   int num_samples = (CONFIG_AUDIO_FRAME_SIZE_SAMPLES * sizeof(int16_t)) / 2;
-  int32_t abs_sum = 0;
+  int64_t abs_sum = 0;
   int16_t *sample_iter = raw_samples;
   for (int i = 0; i < num_samples; ++i) {
-    abs_sum += abs(*sample_iter);
+    abs_sum += (*sample_iter)*(*sample_iter);
     sample_iter++;
   }
-  float avg_amp = abs_sum / (float) (32767 * num_samples);
-  float res = 20*log10(avg_amp);
+  float avg_amp = sqrt(abs_sum / (float) (num_samples));
+  float res = 20*log10(avg_amp / 32767);
 
   //NRF_LOG_DEBUG("noise_level = " NRF_LOG_FLOAT_MARKER "\r\n: ", NRF_LOG_FLOAT(res));
   
   float2Bytes((uint8_t*) p_frame->data, res); // Write the result to the first 4 bytes of output stream.
-  p_frame->data_size = 4; // to represent a float
+  p_frame->data_size = num_samples; // to represent a float
 }
 
 #endif // (CONFIG_AUDIO_CODEC == CONFIG_AUDIO_CODEC_ADPCM)
